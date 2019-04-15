96a8b799e349f5d0d033337359b67e6e1349b202
Chovin Carlson <Chovin@users.noreply.github.com>
2019-04-15 22:12:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,15 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page(url)
+    open(url)
+  end
+  
+  def get_courses
+    
+  end
+  
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
